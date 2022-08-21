786ce56b3f0480e490ff37db700f3c98185d1a73
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-08-20 23:08:20 -0400

    Complete string conversion lesson

--- practice/main.go
+++ practice/main.go
@@ -4,12 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
+	fmt.Print("Enter some text: ")
 
 	input, _ := reader.ReadString('\n')
-	fmt.Println("You entered: ", input)
+	fmt.Println("You entered:", input)
+
+	fmt.Print("Enter a number: ")
+	numberInput, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numberInput), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of number:", aFloat)
+	}
+
 }

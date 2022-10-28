30e095341b55b13bc2bb00c6c2741ecbdc838546
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-10-27 21:01:17 -0700

    Added goose type with no identifiers for fields

--- oop/composition.go
+++ oop/composition.go
@@ -5,15 +5,27 @@ import (
 )
 
 type alive struct {
+}
 
+func (alive) eat() {
+	fmt.Println("Alive and eating")
 }
 
 type walkable struct {
+}
 
+func (alive) walk() {
+	fmt.Println("Alive and strolling through")
 }
 
 type swimmable struct {
+}
 
+func (alive) swim() {
+	fmt.Println("Alive and swimming")
+}
+
+type flyable struct {
 }
 
 type duck struct {
@@ -22,29 +34,34 @@ type duck struct {
 	s swimmable
 }
 
-func (alive) eat() {
-	fmt.Println("Alive and eating")
-}
-func (alive) sleep() {
-	fmt.Println("Alive and sleeping")
-}
-func (alive) walk() {
-	fmt.Println("Alive and strolling through")
-}
-
 func (d duck) eat() {
 	d.a.eat()
 }
 func (d duck) sleep() {
-	d.a.sleep()
+	d.a.swim()
 }
 func (d duck) walk() {
 	d.a.walk()
 }
+
+type goose struct {
+	alive
+	walkable
+	swimmable
+	flyable
+}
+
+func (goose) walk() {
+	fmt.Println("Goose walkin.")
+}
+
 func main() {
 	fmt.Println("Hello, world!")
 	d := duck{}
 	d.eat()
 	d.sleep()
 	d.walk()
+
+	g := goose{}
+	g.walk()
 }

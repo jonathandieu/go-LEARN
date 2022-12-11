17103402e5a0566c6afb568293d0b9460179fd74
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-12-10 17:26:15 -0800

    Add more descriptive comments and start working on implementing variadic funcs with interfaces

--- practice/overload.go
+++ practice/overload.go
@@ -5,6 +5,7 @@ import (
 )
 
 // Takes in one or more ints as parameters
+// This is a variadic function that can take any number of ints as arguments
 func Sum(nums ...int) int {
 	sum := 0
 
@@ -14,6 +15,13 @@ func Sum(nums ...int) int {
 
 	return sum
 }
+
+// func ListyInterface(nums ...interface{}) {
+
+// 	for _, number := range nums {
+// 		sum += number
+// 	}
+// }
 
 func main() {
 	fmt.Println(Sum(6, 9))

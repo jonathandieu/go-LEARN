3df240c20603f95ee602163a7d349320b92efbc0
Jonathan Dieu <jonathanhuydieu@gmail.com>
2024-07-04 08:23:12 -0700

    feat: add function and tests for Spanish and French

--- learn-go-with-tests/helloworld/hello_test.go
+++ learn-go-with-tests/helloworld/hello_test.go
@@ -4,14 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Jon")
+		got := Hello("Jon", "")
 		want := "Hello, Jon!"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, Worlds!"
+		got := Hello("", "")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Spain", "Spanish")
+		want := "Hola, Spain!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("France", "French")
+		want := "Bonjour, France!"
 		assertCorrectMessage(t, got, want)
 	})
 }

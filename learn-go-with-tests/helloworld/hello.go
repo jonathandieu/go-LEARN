c00ef56a889a7ede37428f5cf339930b07793a3f
Jonathan Dieu <jonathanhuydieu@gmail.com>
2024-07-04 07:49:51 -0700

    refactor: update function and test to accept name input and definte constants

--- learn-go-with-tests/helloworld/hello.go
+++ learn-go-with-tests/helloworld/hello.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 )
 
-func Hello() string {
-	return "Hello, world!"
+const englishHelloPrefix = "Hello, "
+func Hello(name string) string {
+	return englishHelloPrefix + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world"))
 }
